f01b7489cfe68c422b8a854b26ad713deb6e2c8a
qinliu777 <1059043813@qq.com>
2016-07-26 09:57:48 +0800

    asd

--- app/src/main/java/com/ecjtu/liuqin/weishi_my/until/adpter/StechListMydapter.java
+++ app/src/main/java/com/ecjtu/liuqin/weishi_my/until/adpter/StechListMydapter.java
@@ -1,7 +1,10 @@
 package com.ecjtu.liuqin.weishi_my.until.adpter;
 
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.ecjtu.liuqin.weishi_my.R;
 import com.ecjtu.liuqin.weishi_my.mode.bean.TranfficBean;
 
 import java.util.List;
@@ -17,6 +20,13 @@ public class StechListMydapter extends CommanAdapter<TranfficBean>{
 
     @Override
     public void current(ViewHolder hodler, Object object) {
-           
+       TranfficBean tranfficBean= (TranfficBean) object;
+
+        ((ImageView)hodler.getView(R.id.list_icon)).setImageDrawable(tranfficBean.getAppicon());
+        ((TextView)hodler.getView(R.id.list_appname)).setText((CharSequence) tranfficBean.getAppName());
+        ((TextView)hodler.getView(R.id.list_total)).setText((CharSequence) tranfficBean.getAppTotal());
+        ((TextView)hodler.getView(R.id.list_upload)).setText((CharSequence) tranfficBean.getAppupLoad());
+        ((TextView)hodler.getView(R.id.list_load)).setText((CharSequence) tranfficBean.getAppload());
+
     }
 }
